b59b19dba20660e1f75d46f2772915186bc68666
Tianle Shu <A00268357@student.tus.ie>
2024-02-14 18:12:34 +0000

    add ui for select order

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -89,4 +89,125 @@ window.onload = function () {
           popupWindow.document.close();
         });
     });
+
+  //order function - read info from card
+  $(document).ready(function () {
+    $(".add-to-cart").on("click", function () {
+      var card = $(this).closest(".card");
+      var coffeeName = card.find(".card-title").text();
+      var coffeePrice = card.find(".card-price").text();
+
+      $("#orderModal #coffeeName").val(coffeeName);
+      $("#orderModal #coffeePrice").val(coffeePrice);
+    });
+  });
+
+  // order function - read info from modal and manage order table
+  $(document).ready(function () {
+    var rowNumber = 1;
+    var totalPrice = 0;
+
+    $(".add-to-cart").on("click", function () {
+      var coffeeName = $(this).data("coffee-name");
+      var coffeePrice = parseFloat($(this).data("coffee-price").substring(1));
+
+      // populate the modal with the coffee name and price
+      $("#orderModal #coffeeName").val(coffeeName);
+      $("#orderModal #coffeePrice").val(coffeePrice);
+
+      // show the modal
+      $("#orderModal").modal("show");
+    });
+
+    function updatePlaceOrderButton() {
+      if ($("#orderTable tbody tr").length > 0) {
+        $("#placeOrder").show();
+      } else {
+        $("#placeOrder").hide();
+      }
+    }
+
+    updatePlaceOrderButton();
+
+    $("#orderModal .btn-primary").on("click", function () {
+      var coffeeName = $("#orderModal #coffeeName").val();
+      var coffeePrice = parseFloat(
+        $("#orderModal #coffeePrice").val().substring(1)
+      );
+      var sizeOption = $("#orderModal #sizeOption").val();
+      var sugarOption = $("#orderModal #sugarOption").val();
+      var milkOption = $("#orderModal #milkOption").val();
+
+      var newRow =
+        "<tr><td class='row-number'>" +
+        rowNumber +
+        "</td><td>" +
+        coffeeName +
+        "</td><td>" +
+        sizeOption +
+        "</td><td>" +
+        sugarOption +
+        "</td><td>" +
+        milkOption +
+        "</td><td class='count'>1</td><td><button class='increase'>+</button><button class='decrease'>-</button><button class='delete'>x</button></td><td class='price' hidden>" +
+        coffeePrice +
+        "</td></tr>";
+
+      $("#orderTable tbody").append(newRow);
+
+      totalPrice += coffeePrice;
+      $("#totalPrice").text("€" + totalPrice.toFixed(2));
+
+      $("#orderModal").modal("hide");
+
+      rowNumber++;
+
+      updatePlaceOrderButton();
+    });
+
+    $("#orderTable").on("click", ".increase", function () {
+      var countCell = $(this).closest("tr").find(".count");
+      var count = parseInt(countCell.text());
+      countCell.text(count + 1);
+
+      var price = parseFloat($(this).closest("tr").find(".price").text());
+      totalPrice += price;
+      $("#totalPrice").text("€" + totalPrice.toFixed(2));
+    });
+
+    $("#orderTable").on("click", ".decrease", function () {
+      var countCell = $(this).closest("tr").find(".count");
+      var count = parseInt(countCell.text());
+      if (count > 1) {
+        countCell.text(count - 1);
+
+        var price = parseFloat($(this).closest("tr").find(".price").text());
+        totalPrice -= price;
+        $("#totalPrice").text("€" + totalPrice.toFixed(2));
+      }
+    });
+
+    $("#orderTable").on("click", ".delete", function () {
+      var count = parseInt($(this).closest("tr").find(".count").text());
+      var price = parseFloat($(this).closest("tr").find(".price").text());
+      totalPrice -= price * count;
+      $("#totalPrice").text("€" + totalPrice.toFixed(2));
+
+      $(this).closest("tr").remove();
+
+      updateRowNumbers();
+      updatePlaceOrderButton();
+    });
+
+    function updateRowNumbers() {
+      $("#orderTable tbody tr").each(function (index) {
+        $(this)
+          .find(".row-number")
+          .text(index + 1);
+      });
+      rowNumber = $("#orderTable tbody tr").length + 1;
+    }
+  });
+
+  // place order function
 };
