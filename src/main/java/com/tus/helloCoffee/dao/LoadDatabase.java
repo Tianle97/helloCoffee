04ed9a214abe29aafbb0d40bfd4bde4ff2839665
Tianle Shu <A00268357@student.tus.ie>
2024-02-14 18:34:33 +0000

    disable auto create objects into database, in case unit test aleays update database

--- src/main/java/com/tus/helloCoffee/dao/LoadDatabase.java
+++ src/main/java/com/tus/helloCoffee/dao/LoadDatabase.java
@@ -14,13 +14,13 @@ import com.tus.helloCoffee.dto.User;
 @Configuration
 public class LoadDatabase {
 
-    @Bean
-    CommandLineRunner initUserDatabase(UserRepository userRepository) {
-        return args -> {
-            userRepository.save(new User("user", "password", "customer"));
-            userRepository.save(new User("admin", "admin", "employee"));
-        };
-    }
+    // @Bean
+    // CommandLineRunner initUserDatabase(UserRepository userRepository) {
+    // return args -> {
+    // userRepository.save(new User("user", "password", "customer"));
+    // userRepository.save(new User("admin", "admin", "employee"));
+    // };
+    // }
 
     // @Bean
     // CommandLineRunner initDatabase(CoffeeRepository repository) {
@@ -58,27 +58,28 @@ public class LoadDatabase {
     // };
     // }
 
-    @Bean
-    CommandLineRunner initOrders(OrderRepository orderRepository,
-    CoffeeRepository coffeeRepository) {
-    return args -> {
-    Order order = new Order("user", "Pending", "Cash", 3.5);
+    // @Bean
+    // CommandLineRunner initOrders(OrderRepository orderRepository,
+    // CoffeeRepository coffeeRepository) {
+    // return args -> {
+    // Order order = new Order("user", "Pending", "Cash", 3.5);
 
-    Coffee co1 = new Coffee("Cappuccino", "Cappuccino.jpg", "Large", "Regular",
-    "Almond milk",
-    "Chocolate powder");
-    Coffee co2 = new Coffee("Espresso", "Espresso.jpg", "Small", "Regular", "No milk", "No extras");
+    // Coffee co1 = new Coffee("Cappuccino", "Cappuccino.jpg", "Large", "Regular",
+    // "Almond milk",
+    // "Chocolate powder");
+    // Coffee co2 = new Coffee("Espresso", "Espresso.jpg", "Small", "Regular", "No
+    // milk", "No extras");
 
-    // Set the Order on the Coffee entities
-    co1.setOrder(order);
-    co2.setOrder(order);
+    // // Set the Order on the Coffee entities
+    // co1.setOrder(order);
+    // co2.setOrder(order);
 
-    // Add the Coffee entities to the Order
-    order.getCoffees().add(co1);
-    order.getCoffees().add(co2);
+    // // Add the Coffee entities to the Order
+    // order.getCoffees().add(co1);
+    // order.getCoffees().add(co2);
 
-    // Save the Order
-    orderRepository.save(order);
-    };
-    }
+    // // Save the Order
+    // orderRepository.save(order);
+    // };
+    // }
 }
